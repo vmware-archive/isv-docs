43e043ff562375f5e1c60b18341d9a25080383e4
Ed Cook <ecook@pivotal.io>
2019-07-16 15:16:39 +0100

    Add tag for docker image in serve script
    - Also switch to more portable shebang
    
    Authored-by: Ed Cook <ecook@pivotal.io>

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-docker run --rm -it -p 8000:8000 -v ${DIR}:/docs dherbrich/mkdocs-material-awesome-pages
+docker run --rm -it -p 8000:8000 -v ${DIR}:/docs dherbrich/mkdocs-material-awesome-pages:0.1.0
